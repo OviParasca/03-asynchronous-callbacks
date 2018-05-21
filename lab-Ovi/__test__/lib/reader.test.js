46f8fb84e6a36b8bef7e7dd75d769c38b3471b6f
Ovi Parasca <OviParasca@gmail.com>
2018-05-20 21:59:15 -0700

    - added missing files and unit tests for this assignment

--- lab-Ovi/__test__/lib/reader.test.js
+++ lab-Ovi/__test__/lib/reader.test.js
@@ -4,21 +4,27 @@ jest.mock('fs');
 
 const reader = require('../../lib/reader.js');
 
+process.argv.push('data/file1.txt');
+process.argv.push('data/file2.txt');
+process.argv.push('data/file3.txt');
+
 describe("File Reader Module", () => {
 
     it('when given a bad file, returns an error', () => {
-        let file = `${__dirname}/../../data/bad.txt`;
-        reader(file, (err, data) => {
+        reader.readFiles(process.argv[0],  (err, data) => {
             expect(err).toBeDefined();
         });
     });
 
-    it('when given a real file, it returns the contents', () => {
-        let file = `${__dirname}/../../data/file1.txt`;
-        reader(file, (err, data) => {
+    it('when given real files, it returns the contents', () => {
+        reader.readFiles([process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7]],  (err, data) => {
             expect(err).toBeUndefined();
             expect(typeof data).toBe("string");
         });
+    });
+
+    it('check if the argv length is greater than 2 before attempting to read files', () => {
+        expect(process.argv.length > 2).toEqual(true);
     });
 
 });
